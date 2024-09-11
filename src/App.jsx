ed68017a147579bed9a4754e03b7255764622eaf
Chaeyoung Shin <scy0723123@gmail.com>
2024-09-11 14:49:50 +0900

    oz-home-style

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import "./App.scss";
+//import "./App.css";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import styled from "styled-components";
+
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  color: white;
+}
+
+body {
+  background-color: #121212;
+}
+
+`;
+
+const Section = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding: 20px 40px;
+`;
 
 function App() {
   return (
     <main>
+      <GlobalStyle />
       <Header />
-      <section>
+      <Section>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </Section>
     </main>
   );
 }
