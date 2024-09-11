05a439ab19c3bc33bfe2dc7b3021411cc4f341e1
Chaeyoung Shin <scy0723123@gmail.com>
2024-09-11 14:52:53 +0900

    oz-home-style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,33 @@
 import styled from "styled-components";
-export default function Header() {
-  const HeaderContainer = styled.header`
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: ${(props) => props.bgColor || "black"};
+
+  ul {
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
-    padding: 10px 20px;
-    background-color: black;
-    ul {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      gap: 20px;
-      li {
-        list-style: none;
-        font-weight: 400;
-        font-size: 13px;
-      }
+    gap: 20px;
+
+    li {
+      list-style: none;
+      font-weight: ${(props) => props.fontWeight || 400};
+      font-size: ${(props) => props.fontSize || "13px"};
     }
-  `;
+  }
+`;
+
+export default function Header({ bgColor, fontWeight, fontSize }) {
   return (
-    <HeaderContainer>
+    <HeaderContainer
+      bgColor={bgColor}
+      fontWeight={fontWeight}
+      fontSize={fontSize}
+    >
       <h1>OZ코딩스쿨</h1>
       <ul className="menu-container">
         <li>로그인</li>
